07a2d8cd575c2c46ecb59936886a316d0aec9070
liljaork <liljaork@gmail.com>
2024-09-08 18:08:27 +0000

    setti upp controller möppu með controller file

---
+++ hugbunadarVerkefni/src/main/java/com/example/hugbunadarVerkefni/controller/controller.java
@@ -0,0 +1,7 @@
+package com.example.hugbunadarVerkefni.controller;
+
+public class controller {
+    public static void main(String[] args) {
+        System.out.println("hæhæ ég er controller");
+    }
+}
